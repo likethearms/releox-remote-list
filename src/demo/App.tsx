652a25073f95aa1e34c307d520e6d2de1895e7d7
Henrik Salmela <henrik.salmela@gmail.com>
2020-11-01 17:58:52 +0200

    Configs

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React from 'react';
 
-function App() {
+const App = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +15,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
